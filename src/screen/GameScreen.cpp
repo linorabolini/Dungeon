439274a1ff47abb7e264b7e1ca52fb52ca765a48
linorabolini <linorabolini@gmail.com>
2016-11-30 11:53:31 +0100

    update

--- src/screen/GameScreen.cpp
+++ src/screen/GameScreen.cpp
@@ -7,7 +7,7 @@ GameScreen::GameScreen()
 
 GameScreen::~GameScreen()
 {
-    delete(board_);
+    delete (board_);
 }
 
 void GameScreen::load()
@@ -22,24 +22,19 @@ void GameScreen::update()
 }
 
 void GameScreen::unload(){};
-void GameScreen::handleInput(sf::RenderWindow *win_){};
+void GameScreen::handleInput(sf::RenderWindow *win_)
+{
+    sf::Event event;
+    while (win_->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            win_->close();
+    }
+};
 
 void GameScreen::render(sf::RenderWindow *win_)
 {
     // sf::CircleShape shape(100.f);
     // shape.setFillColor(sf::Color::Green);
-
-    while (win_->isOpen())
-    {
-        sf::Event event;
-        while (win_->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                win_->close();
-        }
-
-        win_->clear();
-        board_->render(win_);
-        win_->display();
-    }
+    board_->render(win_);
 }
